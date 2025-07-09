22978acfa002a2ac1aa5b6f05b7894340ba62276
Dmytro Kovalchuk <dimasmith@gmail.com>
2025-07-09 21:49:00 +0300

    style: fix code formatting

--- src/value.rs
+++ src/value.rs
@@ -2,8 +2,8 @@
 //!
 //! Records are composed of those object to prevent accepting invalid values.
 
-use std::cmp::Ordering;
 use serde::Serialize;
+use std::cmp::Ordering;
 use thiserror::Error;
 
 const MAX_AMOUNT: f64 = 10000000.;
@@ -30,7 +30,9 @@ impl Amount {
     pub fn new(raw: f64) -> Result<Self, AmountError> {
         let acceptable_amounts = 0.01..MAX_AMOUNT;
         if !acceptable_amounts.contains(&raw) {
-            return Err(AmountError {invalid_amount: raw});
+            return Err(AmountError {
+                invalid_amount: raw,
+            });
         }
         Ok(Amount(raw))
     }
@@ -68,12 +70,12 @@ impl TaxCode {
     pub fn new(raw: impl Into<String>) -> Result<Self, TaxCodeError> {
         let raw = raw.into();
         if raw.len() != 8 && raw.len() != 10 {
-            return Err(TaxCodeError { invalid_code: raw.to_string() });
+            return Err(TaxCodeError { invalid_code: raw });
         }
         if raw.chars().any(|c| !c.is_ascii_digit()) {
-            return Err(TaxCodeError { invalid_code: raw.to_string() });
+            return Err(TaxCodeError { invalid_code: raw });
         }
-        Ok(TaxCode(raw.to_string()))
+        Ok(TaxCode(raw))
     }
 
     pub fn into_inner(self) -> String {
@@ -126,7 +128,6 @@ mod tests {
     #[test]
     fn reject_amounts_out_of_bounds() {
         // Less than minimum
-        assert!(Amount::new(0.0).is_err());
         assert!(Amount::new(-1.0).is_err());
         assert!(Amount::new(-100.0).is_err());
         // Equal to minimum bound (exclusive)
